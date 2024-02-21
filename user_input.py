b6c8c8f0e57efb7254708e938e4172224b7cd022
montfortkorir <ipkipkorir@gmail.com>
2024-02-21 21:45:38 +0300

    completed python  assignment

---
+++ user_input.py
@@ -0,0 +1,10 @@
+#Capture input data from user
+print("Please enter your first name: ")
+name = input()
+print("Please enter your age: ")
+age = input()
+print("Please enter your location: ")
+location = input()
+
+#print out a message containing the user's input
+print("\nHello " + name + ". \nYou are " + age + " years old. \nYou come from " + location + ".\n")
